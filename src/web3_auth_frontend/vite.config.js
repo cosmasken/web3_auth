b8fbe7744f0e58613bfdd22eb9a9bb529e2a4674
cosmasken <cosmasarubaken@gmail.com>
2024-10-05 09:00:18 +0300

    README UPDATE

--- src/web3_auth_frontend/vite.config.js
+++ src/web3_auth_frontend/vite.config.js
@@ -1,21 +1,67 @@
-import { fileURLToPath, URL } from 'url';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
-import dotenv from 'dotenv';
+// import { fileURLToPath, URL } from 'url';
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import environment from 'vite-plugin-environment';
+// import dotenv from 'dotenv';
 
-dotenv.config({ path: '../../.env' });
+// dotenv.config({ path: '../../.env' });
+
+// export default defineConfig({
+//   build: {
+//     emptyOutDir: true,
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       define: {
+//         global: "globalThis",
+//       },
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://127.0.0.1:4943",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   plugins: [
+//     react(),
+//     environment("all", { prefix: "CANISTER_" }),
+//     environment("all", { prefix: "DFX_" }),
+//   ],
+//   resolve: {
+//     alias: [
+//       {
+//         find: "declarations",
+//         replacement: fileURLToPath(
+//           new URL("../declarations", import.meta.url)
+//         ),
+//       },
+//     ],
+//   },
+// });
+
+import path from "path";
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
+import environment from 'vite-plugin-environment';
+import react from '@vitejs/plugin-react';
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
+   root:'.',
   build: {
+    outDir: path.resolve(
+      __dirname,
+      "dist"
+    ),
     emptyOutDir: true,
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-    },
+  define: {
+    global: "window",
   },
   server: {
     proxy: {
@@ -25,19 +71,25 @@ export default defineConfig({
       },
     },
   },
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: "declarations",
+  //       replacement: fileURLToPath(
+  //         new URL("../declarations", import.meta.url)
+  //       ),
+  //     },
+  //   ],
+  // },
   plugins: [
     react(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
-  resolve: {
-    alias: [
-      {
-        find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
-      },
-    ],
-  },
+  // plugins: [
+  //   EnvironmentPlugin("all", { prefix: "CANISTER_" }),
+  //   EnvironmentPlugin("all", { prefix: "DFX_" }),
+  //   EnvironmentPlugin({ BACKEND_CANISTER_ID: "" }),
+  // ],
 });
+
